ba96c28d573d3e87e1690a59cb245864a995acd8
ChristopherPalloArias <167264603+ChristopherPalloArias@users.noreply.github.com>
2025-06-17 16:17:49 -0500

    Include restaurant id in order update topic

--- src/main/java/club/castillo/restaurantes/controller/OrderUpdatesController.java
+++ src/main/java/club/castillo/restaurantes/controller/OrderUpdatesController.java
@@ -1,13 +1,14 @@
 package club.castillo.restaurantes.controller;
 
+import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.SubscribeMapping;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class OrderUpdatesController {
 
-    @SubscribeMapping("/orders")
-    public void subscribe() {
+    @SubscribeMapping("/orders/{restaurantId}")
+    public void subscribe(@DestinationVariable Long restaurantId) {
         // Subscription endpoint for order updates
     }
 }
